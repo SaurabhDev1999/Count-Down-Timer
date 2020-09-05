7bd4baa74ec366b8dff5daf80d6cf5e36b8fce30
SaurabhDev1999 <53016632+SaurabhDev1999@users.noreply.github.com>
2020-09-05 16:01:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const timerDayEl = document.querySelector('.timer__day');
+const timerHourEl = document.querySelector('.timer__hour');
+const timerMinEl = document.querySelector('.timer__min');
+const timerSecEl = document.querySelector('.timer__sec');
+
+function getTimeDifference(start, end) {
+  const milliseconds = Math.floor(end - start);
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  hours -= days * 24;
+  minutes = minutes - days * 24 * 60 - hours * 60;
+  seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+
+  return {
+    rDays: days,
+    rHours: hours,
+    rMinutes: minutes,
+    rSeconds: seconds,
+  };
+}
+
+const timer = setInterval(function() {
+  const startDate = new Date();
+  const endDate = new Date('August 30,2020 07:30:00');
+
+  const timeDifferenceObj = getTimeDifference(startDate, endDate);
+  timerDayEl.textContent = timeDifferenceObj.rDays;
+  timerHourEl.textContent = timeDifferenceObj.rHours;
+  timerMinEl.textContent = timeDifferenceObj.rMinutes;
+  timerSecEl.textContent = timeDifferenceObj.rSeconds;
+}, 1000);
